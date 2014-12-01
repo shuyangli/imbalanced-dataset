81fe4c4e9af1f42a570b0c95183bfcc1bf6bee53
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2014-11-30 19:53:07 -0500

    Add Select Box for selection of Classifiers and Datasets.

--- frontend/app/controllers/dashboard.js
+++ frontend/app/controllers/dashboard.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  datafiles: Ember.computed.mapBy('datasets', 'names'),
+  classifier_files: Ember.computed.mapBy('classifiers', 'names')
 });
