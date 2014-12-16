959af86ec2898227b05f24017736c923b3f448e6
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2014-12-15 22:30:26 -0500

    More changes to database and improvements.

--- frontend/app/controllers/dashboard.js
+++ frontend/app/controllers/dashboard.js
@@ -3,18 +3,22 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   datafiles: Ember.computed.mapBy('datasets', 'names'),
   classifier_files: Ember.computed.mapBy('classifiers', 'names'),
-  selectedDatasets: [],
-  selectedClassifiers: [],
+  fields: {},
+  //selectedDatasets: [],
+  //selectedClassifiers: [],
   selectedOutput: null,
   emptyDatasets: function() {
-    var sel_datasets = this.get('selectedDatasets');
-    var sel_classifiers = this.get('selectedClassifiers');
+    var fields = this.get('fields');
+    console.log(fields);
+    var sel_datasets = fields.selectedDatasets;
+    var sel_classifiers = fields.selectedClassifiers; //this.get('fields.selectedClassifiers');
+
     console.log("Empty datasets!");
     console.log(sel_datasets.length);
     console.log(sel_classifiers.length);
 
     return (sel_datasets.length === 0 || sel_classifiers.length === 0);
-  }.property('selectedDatasets', 'selectedClassifiers'),
+  }.property('fields', 'fields.selectedDatasets', 'fields.selectedClassifiers'),
   actions: {
     submit: function() {
       //alert("Form submitted with the following files!");
@@ -22,20 +26,24 @@ export default Ember.Controller.extend({
       console.log(this.get('selectedDatasets'));
       var selected_file = this.get('selectedDatasets');
       var selected_classifiers = this.get('selectedClassifiers');
+
+      var fields = this.get('fields');
+      console.log(fields);
       console.log(selected_classifiers);
       console.log(selected_file);
+      fields.classifier_ids = fields.selectedClassifiers;
 
-      var analysis = this.store.createRecord('analysis', {
-        'title': "Testing",
-        'classifier_ids': selected_classifiers,
-      });
+      var analysis = this.store.createRecord('analysis', fields);
 
       //analysis.save();
 
-      this.store.find('dataset', selected_file).then(function(file) {
+      this.store.find('dataset', fields.selectedDatasets).then(function(file) {
         console.log("Analysis set.");
         analysis.set('dataset', file);
-        analysis.save();
+        analysis.save().then(function(response) {
+          console.log("Response!");
+          console.log(response);
+        });
       });
 
 
--- frontend/app/models/analysis.js
+++ frontend/app/models/analysis.js
@@ -8,6 +8,8 @@ export default DS.Model.extend({
   classifiers: DS.hasMany('classifier', {async: true}),
   title: DS.attr('string'),
   classifier_ids: DS.attr(),
+  hasHeader: DS.attr('boolean'),
+  posLabel: DS.attr('number'),
   completed: DS.attr('boolean'),
   created: DS.attr('date')
 });
