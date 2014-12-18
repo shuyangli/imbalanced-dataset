65c3feabbbb8a553d723228d23a79b38b52a5569
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2014-12-17 21:23:39 -0500

    Add support for execution and saving of multiple classifiers.

--- frontend/app/models/analysis.js
+++ frontend/app/models/analysis.js
@@ -4,12 +4,14 @@ var inflector = Ember.Inflector.inflector;
 inflector.irregular('analysis', 'analyses');
 
 export default DS.Model.extend({
-  dataset: DS.belongsTo('dataset'),
+  dataset: DS.belongsTo('dataset', {async: true}),
   classifiers: DS.hasMany('classifier', {async: true}),
   title: DS.attr('string'),
+  description: DS.attr('string'),
   classifier_ids: DS.attr(),
   hasHeader: DS.attr('boolean'),
-  posLabel: DS.attr('number'),
+  posLabel: DS.attr('number', {default: 1}),
   completed: DS.attr('boolean'),
+  ignoreFirst: DS.attr('boolean'),
   created: DS.attr('date')
 });
--- frontend/app/routes/dashboard.js
+++ frontend/app/routes/dashboard.js
@@ -7,10 +7,12 @@ export default Ember.Route.extend({
 
   setupController: function(controller, model) {
      this._super(controller,model);
+
      controller.set('fields', {
       selectedDatasets: [],
       selectedClassifiers: [],
      });
+
      controller.set('datasets', this.store.find('dataset'));
      controller.set('classifiers', this.store.find('classifier'));
      controller.set('users', this.store.find('user'));
